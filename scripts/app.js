ff3ade185c5113db966c80a1dd9fbc6706cd59b7
azeneli <azeneli46@gmail.com>
2016-01-11 18:16:01 -0500

    Selected canvas, set width & height and created a rectangle inside

--- scripts/app.js
+++ scripts/app.js
@@ -2,8 +2,34 @@
 
 (function(){ // protect the lemmings
 
-		
-	
+  // Creating the canvas	
+
+	var $canvas = $("#js-Snake");
+	// console.log( $canvas );
+	$canvas.attr('width', 500);  // set width
+	$canvas.attr('height', 500); // set height
+	var canvas = $canvas[0]; // grab the DOM el
+	// console.log( canvas );
+	var context = canvas.getContext('2d'); // grab the context
+	// color of rectangle
+	context.fillStyle = 'red';
+	// defined pos of rectangle(x,y,width, height)
+	context.fillRect(5,10,30,50); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
