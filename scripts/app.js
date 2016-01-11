40b99b209721856095da393df7aaf21093a03129
azeneli <azeneli46@gmail.com>
2016-01-11 18:35:59 -0500

    Created game loop. Code architecture set up using reveling module pattern

--- scripts/app.js
+++ scripts/app.js
@@ -1,39 +1,38 @@
 /* CURRENTLY IN: scripts/app.js */
 
-(function(){ // protect the lemmings
+var __JS__SNAKE__ = {};
 
-  // Creating the canvas	
+__JS__SNAKE__.game = (function(){
 
-	var $canvas = $("#js-Snake");
-	// console.log( $canvas );
-	$canvas.attr('width', 500);  // set width
-	$canvas.attr('height', 500); // set height
-	var canvas = $canvas[0]; // grab the DOM el
-	// console.log( canvas );
-	var context = canvas.getContext('2d'); // grab the context
-	// color of rectangle
-	context.fillStyle = 'red';
-	// defined pos of rectangle(x,y,width, height)
-	context.fillRect(5,10,30,50); 
+	var context,
+		xPos = 0,
+		yPos = 0,
+		frameLen = 	300; // new frame every sec
 
+	function init(){
+		var $canvas = $("#js-Snake");
+		$canvas.attr('width', 500);  // set width
+		$canvas.attr('height', 500); // set height
+		var canvas = $canvas[0]; // grab the DOM el
+		context = canvas.getContext('2d'); // grab the context
+		gameLoop();
+	}
 
+	// called at a fixed interval to update the state of the game and display the changes
+	function gameLoop(){
+		xPos += 2;
+		yPos += 4;
+		context.clearRect(0,0,500,500);
+		context.fillStyle = '#fe57a1';
+		context.fillRect(xPos, yPos, 30, 50); // moving rect
+		setTimeout(gameLoop, frameLen); // reset
+	}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	return {
+		init: init
+	};
 
 })();
+
+__JS__SNAKE__.game.init();
+
