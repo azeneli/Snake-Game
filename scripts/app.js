c138736029439648b3eee763eea8ae53948bbb7a
azeneli <azeneli46@gmail.com>
2016-01-13 20:08:32 -0500

    Add comments explaining code

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,5 @@
 /* CURRENTLY IN: scripts/app.js */
+/* Revealing Module Pattern Example */
 
 var __JS__SNAKE__ = {};
 
@@ -11,21 +12,28 @@ __JS__SNAKE__.game = (function(){
 
 	function init(){
 		var $canvas = $("#js-Snake");
+		console.log( $canvas );
 		$canvas.attr('width', 500);  // set width
 		$canvas.attr('height', 500); // set height
 		var canvas = $canvas[0]; // grab the DOM el
+		console.log( canvas );
 		context = canvas.getContext('2d'); // grab the context
 		gameLoop();
 	}
 
 	// called at a fixed interval to update the state of the game and display the changes
 	function gameLoop(){
+		// update state
 		xPos += 2;
 		yPos += 4;
+
+		// update display
 		context.clearRect(0,0,500,500);
-		context.fillStyle = '#fe57a1';
-		context.fillRect(xPos, yPos, 30, 50); // moving rect
-		setTimeout(gameLoop, frameLen); // reset
+		context.fillStyle = '#fe57a1'; // set fill style of context 
+		context.fillRect(xPos, yPos, 30, 50); // fill rec, (x,y, width, height) -- in this case moving rectangle
+
+		// pass game loop function & wait for it to be called again
+		setTimeout(gameLoop, frameLen); 
 	}
 
 	return {
